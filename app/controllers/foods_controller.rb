9751ad30ad9d113150a79a16e45a58cfe21330d1
Patrick Duvall <duvallp208@gmail.com>
2019-07-08 09:31:18 -0600

    Read Api docs, begining to to query

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,7 +1,7 @@
-def FoodsController < ApplicationController
-
-
+class FoodsController < ApplicationController
   def index
-    
+
+    conn = Faraday.get("https://api.nal.usda.gov/ndb/search/?format=json&q=#{params['q']}&sort=n&max=10&offset=0&api_key=lg6h9g1vaB3tS1nGB9JmRAYzddGGus1AL1eCNJcT")
+    require "pry"; binding.pry
   end
 end
